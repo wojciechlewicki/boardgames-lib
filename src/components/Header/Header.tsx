f7f7d92e854b50159328cb5cd83b22f45414b12b
wojciechlewicki <78853888+wojciechlewicki@users.noreply.github.com>
2022-09-04 18:20:00 +0200

    navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
+import SearchForm from "./Navigation/SearchForm";
+
 import { ReactComponent as Logo } from "../../assets/Logo.svg";
 import { ReactComponent as Hamburger } from "../../assets/Hamburger_menu.svg";
-//import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 import classes from "./Header.module.css";
 import Wrapper from "../ui/Wrapper";
@@ -10,11 +12,18 @@ const Header = () => {
     <header className={`${classes.header} outer-wrapper`}>
       <Wrapper>
         <div className="flex-row-center">
-          <Hamburger />
-          <Logo />
-          <button>Sign In</button>
-          <button>Sign Up</button>
+          <div className="flex-row-center">
+            <Hamburger className={classes.headerIcon} />
+            <Logo />
+          </div>
+          <div className="flex-row-center">
+            <Link to="" className={classes.headerIcon}>
+              Sign In
+            </Link>
+            <Link to="">Sign Up</Link>
+          </div>
         </div>
+        <SearchForm />
       </Wrapper>
     </header>
   );
