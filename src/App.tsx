d44c407829f3e9991032fecf842cf9f45c9a23ad
wojciechlewicki <78853888+wojciechlewicki@users.noreply.github.com>
2022-09-12 18:09:11 +0200

    added reduxjs/toolkit and other dep

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { Fragment } from 'react';
-import Header from './components/Header';
+import { Fragment } from "react";
+import Header from "./components/Header";
 
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+
+import classes from "./App.module.css";
 
 function App() {
   return (
     <Fragment>
       <Header></Header>
+      <main className={`${classes.main} outer-wrapper`}>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </main>
     </Fragment>
   );
 }
